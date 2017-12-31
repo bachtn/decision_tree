16ec2207ecb177b9d1c62836ca3096a4facdbef3
Alaa Bakhti <alaa.bakhti@epita.fr>
2017-12-31 12:48:18 +0100

    [SPLIT-METRICS][GINI-IMPURITY] added gini_impurity functions, not tested yet

--- src/split_metrics/gini_impurity.py
+++ src/split_metrics/gini_impurity.py
@@ -2,27 +2,6 @@ import operator
 
 from tools.tools import is_continuous
 
-def get_gini_split(attribute_vector, target_vector):
-    """ Returns the gini split score
-    If the data is continuous, then it returns the best split candidate
-    with its gini split score
-    Otherwise, it returns only the split score
-    """
-    parent_gini_idx = get_gini_index(target_vector)
-    n = len(attribute_vector)
-    records_gini_idx = 0
-    if is_continuous(attribute_vector):
-        candidate_split, gini_split = get_best_candidate_split(attribute_vector, target_vector, parent_gini_idx)
-        return gini_split, candidate_split
-    else:
-        for value, nb_occur in attribute_vector.value_counts().items():
-            current_record_gini_idx = get_gini_index(target_vector[attribute_vector == value])
-            prob =  nb_occur / n
-            records_gini_idx += prob * current_record_gini_idx
-    
-        gini_split = parent_gini_idx - records_gini_idx
-    return gini_split
-
 def get_gini_index(target_vector):
     accuracy = 0
     n = len(target_vector)
@@ -31,16 +10,65 @@ def get_gini_index(target_vector):
         accuracy += prob ** 2
     return 1 - accuracy
 
-def get_split_candidates(values):
-    possible_thresholds = []
-    values.sort()
-    for i in range(len(values) - 1):
-        mid = (values[i] + values[i+1]) / 2
-        possible_thresholds.append(mid)
-    return possible_thresholds
+def get_gini_split(attribute_vector, target_vector):
+    """ Returns the gini split score
+    @return:
+        - data = continuous  -> (split_score, split_candidate)
+        - data = categorical -> (split_score, None)
+    """
+    candidate_split = None
+    parent_gini_idx = get_gini_index(target_vector)
+    if is_continuous(attribute_vector):
+        records_gini_idx, candidate_split = get_continuous_gini_index(
+                attribute_vector, target_vector, metric='brute_force')
+    else:
+        records_gini_idx = get_categorical_gini_index(
+                attribute_vector, target_vector)
+    gini_split = parent_gini_idx - records_gini_idx
+    return (gini_split, candidate_split)
 
+def get_categorical_gini_index(attribute_vector, target_vector):
+    n = len(attribute_vector)
+    records_gini_idx = 0
+    for value, nb_occur in attribute_vector.value_counts().items():
+        current_record_gini_idx = get_gini_index(target_vector[attribute_vector == value])
+        prob =  nb_occur / n
+        records_gini_idx += prob * current_record_gini_idx
+    return records_gini_idx
 
-def get_gini_index_continuous(attribute_vector, target_vector, candidate_split):
+def get_continuous_gini_index(attribute_vector, target_vector,
+        metric='naive'):
+    if metric == 'naive':
+        records_gini_idx, candidate_split = naive_gini(
+                attribute_vector, target_vector)
+    elif metric == 'brute_force':
+        records_gini_idx, candidate_split = brute_force_gini(
+                attribute_vector, target_vector)
+    else:
+        records_gini_idx, candidate_split = naive_gini(
+                attribute_vector, target_vector)
+    return records_gini_idx, candidate_split
+
+def brute_force_gini(attribute_vector, target_vector):
+    split_candidates = get_split_candidates(attribute_vector.values)
+    candidates_gini_score = []
+    for candidate in split_candidates:
+        candidate_gini_index = get_candidate_gini_index(
+                attribute_vector, target_vector, candidate)
+        candidates_gini_score.append(candidate_gini_index)
+    best_gini_idx = min(candidates_gini_score)
+    best_candidate_split = split_candidates[
+            candidates_gini_score.index(best_gini_idx)]
+    return best_gini_idx, best_candidate_split
+
+def naive_gini(attribute_vector, target_vector):
+    """ Return the median as a candidate_split """
+    candidate_split = attribute_vector.median()
+    records_gini_idx = get_candidate_gini_index(
+            attribute_vector, target_vector, candidate_split)
+    return records_gini_idx, candidate_split
+
+def get_candidate_gini_index(attribute_vector, target_vector, candidate_split):
     records_gini_idx = 0
     n = len(attribute_vector)
     operators = [operator.le, operator.gt]
@@ -50,13 +78,15 @@ def get_gini_index_continuous(attribute_vector, target_vector, candidate_split):
         records_gini_idx += prob * record_gini_idx
     return records_gini_idx
 
-def get_best_candidate_split(attribute_vector, target_vector, parent_gini_idx):
-    candidates_gini_split = []
-    split_candidates = get_split_candidates(attribute_vector.values)
-    for candidate in split_candidates:
-        gini_idx = get_gini_index_continuous(attribute_vector, target_vector, candidate)
-        gini_split = parent_gini_idx - gini_idx
-        candidates_gini_split.append(gini_split)
-    best_gini_split = max(candidates_gini_split)
-    best_candidate_split = split_candidates[candidates_gini_split.index(best_gini_split)]
-    return best_candidate_split, best_gini_split
+def get_split_candidates(values):
+    """ 
+    - [1,5,3]
+    - Sort the values -> [1,3,5]
+    - For each pair of values, get the mid value -> [2,4]
+    """
+    split_candidates = []
+    values.sort()
+    for i in range(len(values) - 1):
+        mid = (values[i] + values[i+1]) / 2
+        split_candidates.append(mid)
+    return split_candidates
